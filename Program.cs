1b712ac8096e2587a76b0ae27f4bd9175724e939
bradygrapentine <brady.grapentine@gmail.com>
2021-04-29 10:20:32 -0400

    Set up database context

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 namespace SuncoastMovies
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Movie
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string PrimaryDirector { get; set; }
+        public int YearReleased { get; set; }
+        public string Genre { get; set; }
     }
-  }
+    class SuncoastMoviesContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
 }
